109dde4d5f70f8ee94c7ca509603de4b05dbd09e
csx1_team <cmazzoli@bnl.gov>
2019-11-19 10:02:15 -0500

    ENH: add interpolator controls to EPUs

--- profile_collection/startup/csx1/devices/epu.py
+++ profile_collection/startup/csx1/devices/epu.py
@@ -15,6 +15,23 @@ class EPUMotor(PVPositionerPC):
         self._stop_suffix = stop_suffix
         super().__init__(*args, parent=parent, **kwargs)
 
+
+class Interpolator(Device):
+    input = Cpt(EpicsSignal, 'Val:Inp1-SP')
+    input_offset = Cpt(EpicsSignal, 'Val:InpOff1-SP')
+    # {'Enabled', 'Disabled'}
+    input_link = Cpt(EpicsSignal, 'Enbl:Inp1-Sel', string=True)
+    input_pv = Cpt(EpicsSignal, 'Val:Inp1-SP.DOL$', string=True)
+    output = Cpt(EpicsSignalRO, 'Val:Out1-I')
+    # {'Enable', 'Disable'}
+    output_link = Cpt(EpicsSignalRO, 'Enbl:Out1-Sel', string=True)
+    output_pv = Cpt(EpicsSignal, 'Calc1.OUT$', string=True)
+    output_deadband = Cpt(EpicsSignal, 'Val:DBand1-SP')
+    output_drive = Cpt(EpicsSignalRO, 'Val:OutDrv1-I')
+    interpolation_status = Cpt(EpicsSignalRO, 'Sts:Interp1-Sts', string=True)
+#    table = Cpt(EpicsSignal, 'Val:Table-Sel', name='table')
+
+
 class EPU(Device):
     gap = Cpt(EPUMotor, '-Ax:Gap}', stop_suffix='-Ax:Gap}')
     phase = Cpt(EPUMotor, '-Ax:Phase}', stop_suffix='-Ax:Phase}')
@@ -22,9 +39,10 @@ class EPU(Device):
     x_ang = FmCpt(EpicsSignalRO,'{self._ai_prefix}:FPGA:x_mrad-I')
     y_off = FmCpt(EpicsSignalRO,'{self._ai_prefix}:FPGA:y_mm-I')
     y_ang = FmCpt(EpicsSignalRO,'{self._ai_prefix}:FPGA:y_mrad-I')
+    flt = Cpt(Interpolator, '-FLT}')
+    rlt = Cpt(Interpolator, '-RLT}')
 
     def __init__(self, *args, ai_prefix=None, epu_prefix=None, **kwargs):
         self._ai_prefix = ai_prefix
         self._epu_prefix = epu_prefix
         super().__init__(*args, **kwargs)
-
