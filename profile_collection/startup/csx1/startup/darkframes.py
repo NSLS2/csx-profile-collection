b53111ebc22e5513064b366a7ea6f0a690b5d40b
csx1_team <cmazzoli@bnl.gov>
2020-01-15 17:23:50 -0500

    dan's changes for num images dependant on exposure time.

--- profile_collection/startup/csx1/startup/darkframes.py
+++ profile_collection/startup/csx1/startup/darkframes.py
@@ -8,18 +8,47 @@ from ..startup.optics import inout
 
 
 class TemporarilyCustomizedDarkFrameProcessor(DarkFramePreprocessor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._disabled = False
+
+    def __repr__(self):
+        return f"<DarkFramePreprocessor {len(self.cache)} snapshots cached>"
+
     def disable(self):
-        ...
+        self._disabled = True
 
     def enable(self):
-        ...
+        self._disabled = False
+
+    def __call__(self, plan):
+        if self._disabled:
+            return (yield from plan)
+        else:
+            return (yield from super().__call__(plan))
 
 
+def num_images_heuristic(acquire_time):
+    """
+    How many dark frame exposures should I take?
+
+    Given an acquire time, provide an approriate estimate for the number of
+    dark frame exposures to take.
+
+    This takes into account the readout error and the dark current.
+    """
+    # This algorithm is based on a rough heuristic and could be
+    # improved later.
+    lookup = {0: 100, 0.1: 100, 0.5: 30, 1.0: 50, 3.0: 20}
+    for t, num in lookup.items():
+        if acquire_time < t:
+            break
+    return num
 
 
-def dark_plan(gain, num_dark_images):
+def dark_plan(gain):
+    print('Taking a dark frame...')
     # Close shutter
-    ...
     yield from bps.mv(inout,'In')
 
 
@@ -35,6 +64,13 @@ def dark_plan(gain, num_dark_images):
     # Structure of reading is
     # {fccd.cam.num_images.name: {'value': ..., 'timestamp': ...}}
     original_num_images = reading[fccd.cam.num_images.name]['value']
+
+    # Decide on a good num_images setting based on the current exposure
+    # time.
+    reading = yield from bps.read(fccd.cam.acquire_time)
+    current_exposure_time = reading[fccd.cam.acquire_time.name]['value']
+
+    num_dark_images = num_images_heuristic(current_exposure_time)
 
     # We need to put the dark frames in a separate Resource because
     # it may have a different 'frames_per_point' than the light frames.
@@ -72,6 +108,7 @@ def dark_plan(gain, num_dark_images):
     yield from bps.sleep(2)
 
     #Im not sure that this is the best place, then it is open /close alot and we should turn the feedback on first
+    print('Dark frame complete. Proceeding....')
 
     return snapshot
 
@@ -85,14 +122,18 @@ inout_sate = inout.status.value
 
 #for gain in (1, 2, 8):
 #for gain in (0, 1, 2):
+#for gain in (0, 1):  #TODO for testing multiple darks
 for gain in (0,):
     # 1, 2, and 8 are the multipliers.  auto = 8,
     #gain_bit_dict = {0:'auto', 1: 'x2', 2: 'x1'} #where the keys are the epics values
-    NUM_DARK_IMAGES = 10
-    this_dark_plan = functools.partial(dark_plan, gain, NUM_DARK_IMAGES)
+    this_dark_plan = functools.partial(dark_plan, gain)
 
-    dark_frame_preprocessor = DarkFramePreprocessor(
-        dark_plan=this_dark_plan, detector=fccd, max_age=600)
+    dark_frame_preprocessor = TemporarilyCustomizedDarkFrameProcessor(
+        dark_plan=this_dark_plan, detector=fccd, max_age=600,
+        locked_signals=(fccd.cam.acquire_time,),
+        stream_name=f'dark')
+        #stream_name=f'dark_{gain}')  #TODO for testing multiple darks
+
     dark_frame_preprocessors[gain] = dark_frame_preprocessor
 
 
