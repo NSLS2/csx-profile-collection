826859257c9c424d0b02e6dc8d6be6c9df95a177
Andi Barbour <abarbour@bnl.gov>
2020-03-06 11:42:26 -0500

    new nanops and Maksim hack for open loop problem

--- profile_collection/startup/02-nanops.py
+++ profile_collection/startup/02-nanops.py
@@ -3,7 +3,8 @@ from ophyd.device import Component as Cpt
 from ophyd.signal import EpicsSignal
 from ophyd.status import (MoveStatus, DeviceStatus, wait as status_wait, StatusBase)
 from ophyd import MotorBundle
-from epics import caget,caput
+from ophyd.utils.epics_pvs import raise_if_disconnected
+#from epics import caget,caput
 
 
 class NanoMotor(EpicsMotor):
@@ -14,8 +15,8 @@ class NanoMotor(EpicsMotor):
 
     _default_configuration_attrs = (
         epics_config_attrs +
-        ['dly', 'rtry', 'rdbd', 'rmod', 'cnen', 'pcof', 'icof'])
-    user_setpoint = Cpt(EpicsSignal, 'PA_sm')
+        ['dly', 'rtry', 'rdbd', 'rmod', 'cnen', 'pcof', 'icof'])#TODO ensure not part of baseline data
+    #user_setpoint = Cpt(EpicsSignal, 'PA_sm') #not v3 asmbly epics. .VAL should be
     dly = Cpt(EpicsSignal, '.DLY')
     rtry = Cpt(EpicsSignal, '.RTRY')
     rdbd = Cpt(EpicsSignal, '.RDBD')
@@ -24,7 +25,8 @@ class NanoMotor(EpicsMotor):
     pcof = Cpt(EpicsSignal, '.PCOF')
     icof = Cpt(EpicsSignal, '.ICOF')
 
-class NanoMotorOpenLoop(EpicsMotor):
+
+class NanoMotorOpenLoop(EpicsMotor): #TODO unverified for v3 asmbly epics
     #_default_configuration_attrs = (
     #    EpicsMotor._default_configuration_attrs +
     #    ('dly', 'rdbd', 'rmod', 'cnen', 'pcof', 'icof'))
@@ -57,40 +59,54 @@ class NanoMotorOpenLoop(EpicsMotor):
                 self._signals.pop(k, None)
         print(f'Signals: {list(self._signals.keys())}')
 
-    def set(self, value):
-        #self.done_signal.value=1
-        st = DeviceStatus(self)
-        # these arg sames matter
-        #def am_done(old_value, value, **kwargs):
-        def am_done(old_value,value, **kwargs):
-            #if old_value == 1 and value == 0:
-            #print("running",old_value,value,self.done_signal.value)
-            #print("done_signal: ",smtr.done_signal.value)
-            """
-            if self.done_signal.value==1:
-                if self.last_value==0:
-                    st._finished()
-                self.last_value=1
-            else:
-                self.last_value=0
-            """
-            if old_value==0 and value==1:
-                st._finished()
+#COMMENT BELOW FOR NEW NANOP TESTING
+    #def set(self, value):
+    #    #self.done_signal.value=1
+    #    st = DeviceStatus(self)
+    #    # these arg sames matter
+    #    #def am_done(old_value, value, **kwargs):
+    #    def am_done(old_value,value, **kwargs):
+    #        #if old_value == 1 and value == 0:
+    #        #print("running",old_value,value,self.done_signal.value)
+    #        #print("done_signal: ",smtr.done_signal.value)
+    #        """
+    #        if self.done_signal.value==1:
+    #            if self.last_value==0:
+    #                st._finished()
+    #            self.last_value=1
+    #        else:
+    #            self.last_value=0
+    #        """
+    #        if old_value==0 and value==1:
+    #            st._finished()
 
-        self.done_signal.subscribe(am_done, run=False)
-        self.user_setpoint.set(value)
-        return st
+    #    self.done_signal.subscribe(am_done, run=False)
+    #    self.user_setpoint.set(value)
+    #    return st
+
+
+class NanoMotorWithGentleStop(NanoMotor):
+    @raise_if_disconnected
+    def stop(self, *, success=False):
+        # print(f'\n\n{self.name} motor status: {success}\n\n')
+        # The status is always 'True' even on double Ctrl+C,
+        # that is why we are using an extra condition to check
+        # if the motor is still moving, to stop it.
+        if not success or self.motor_is_moving.get():
+            self.motor_stop.put(1, wait=False)
+            super().stop(success=success)
 
 
 class NanoBundle(MotorBundle):
-    tx = Cpt(NanoMotor, 'TopX}Mtr')
-    ty = Cpt(NanoMotor, 'TopY}Mtr')
-    tz = Cpt(NanoMotor, 'TopZ}Mtr')
-    bx = Cpt(NanoMotor, 'BtmX}Mtr')
-    by = Cpt(NanoMotor, 'BtmY}Mtr')
-    #swap between the two following line if BtmZ close/open loop is desired, respectively
-    bz = Cpt(NanoMotor, 'BtmZ}Mtr')
-    #bz = Cpt(NanoMotorOpenLoop, 'BtmZ}OL')
+    #tx = Cpt(NanoMotor, 'TopX}Mtr') # essentially open loop mode
+    tx = Cpt(NanoMotorWithGentleStop, 'TopX}Mtr')
+    ty = Cpt(NanoMotorWithGentleStop, 'TopY}Mtr')
+    tz = Cpt(NanoMotorWithGentleStop, 'TopZ}Mtr')
+    bx = Cpt(NanoMotorWithGentleStop, 'BtmX}Mtr')
+    by = Cpt(NanoMotorWithGentleStop, 'BtmY}Mtr')
+    bz = Cpt(NanoMotorWithGentleStop, 'BtmZ}Mtr')
+    #bz = Cpt(NanoMotorOpenLoop, 'BtmZ}OL') #TODO no testing made for v3 asmbly epics
+    #swap between the two above lines if BtmZ close/open loop is desired, respectively #TODO if this is still needed, make this OL part of the device, no restart bsui
 
 
 # check if nanop already there and remove it
@@ -157,28 +173,30 @@ mpz.bz.kind = 'hinted'
 # size and had difficulty in that bluesky will never trigger the
 # cube_beam because bsui is waiting on this to move??
 
-# Settling time (tested 0.1 - 0.3)
-temp_settle_time = 0.2
 
-_base_nano_setting = {'velocity': 0.10,
-                      'acceleration': 0.20,
-                      'dly': temp_settle_time,
-                      'rtry': 3,
-                      'rdbd': 1e-5,
-                      'rmod': 1,
-                      'cnen': 1,
-                      'pcof': 0.1,
-                      'icof': 0.010
-                      }
+# Settling time (tested 0.1 - 0.3)
+#temp_settle_time = 0.2
+#COMMENT BELOW FOR NEW NANOP TESTING
+#_base_nano_setting = {'velocity': 0.10,
+#                      'acceleration': 0.20,
+#                      'dly': temp_settle_time,
+#                      'rtry': 3,
+#                      'rdbd': 1e-5,
+#                      'rmod': 1,
+#                      'cnen': 1,
+#                      'pcof': 0.1,
+#                      'icof': 0.010
+#                      }
 
 for cpt in ['tx', 'ty', 'tz', 'bx', 'by', 'bz']:
     getattr(nanop, cpt).configuration_attrs.extend(['velocity', 'acceleration'])
 
-for nn in nanop.component_names:
-    if nn == "bz (remove this if open loop)":
-        getattr(nanop, nn).configure({'velocity':0.10,'t_settle':temp_settle_time})
-        continue
-    getattr(nanop, nn).configure(_base_nano_setting)
+#COMMENT BELOW FOR NEW NANOP TESTING
+#for nn in nanop.component_names:
+#    if nn == "bz (remove this if open loop)":
+#        getattr(nanop, nn).configure({'velocity':0.10,'t_settle':temp_settle_time})
+#        continue
+#    getattr(nanop, nn).configure(_base_nano_setting)
 
 # BlueskyMagics.positioners += [getattr(nanop, nn) for nn in nanop.component_names]
 
