3326c70f9ca97eaa8fb399a1d543bcdafc3de82c
Maksim Rakitin <mrakitin@bnl.gov>
2021-02-02 18:39:45 -0500

    EpicsSignalBase timeouts

--- profile_collection/startup/00-nsls2-tools.py
+++ profile_collection/startup/00-nsls2-tools.py
@@ -1,17 +1,43 @@
-# DAMA hack on 2019-09-30 to avoid timeouts with the mono motor
-# components:
-# FailedStatus: MoveStatus(done=True, pos=pgm_energy, elapsed=0.4, success=False, settle_time=0.0)
-#
-# /opt/conda_envs/collection-2019-3.0-csx/lib/python3.7/site-packages/epics/ca.py:1459: UserWarning: ca.get('XF:23ID1-OP{Mono-Ax:GrtP}Mtr.TDIR') timed out after 1.00 seconds.
-#   warnings.warn(msg % (name(chid), timeout))
-# /opt/conda_envs/collection-2019-3.0-csx/lib/python3.7/site-packages/epics/ca.py:1459: UserWarning: ca.get('XF:23ID1-OP{Mono-Ax:GrtP}Mtr.HLS') timed out after 1.00 seconds.
-#   warnings.warn(msg % (name(chid), timeout))
-##
-## import ophyd
-## import functools
-## ophyd.signal.EpicsSignalBase.wait_for_connection = functools.partialmethod(ophyd.signal.EpicsSignalBase.wait_for_connection, timeout=5)
-## print(ophyd.signal.EpicsSignalBase.wait_for_connection.__dict__)
-## It did not help, commenting it back.
+###############################################################################
+# TODO: remove this block once https://github.com/bluesky/ophyd/pull/959 is
+# merged/released.
+from datetime import datetime
+from ophyd.signal import EpicsSignalBase, EpicsSignal, DEFAULT_CONNECTION_TIMEOUT
+
+def print_now():
+    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
+
+def wait_for_connection_base(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
+    '''Wait for the underlying signals to initialize or connect'''
+    if timeout is DEFAULT_CONNECTION_TIMEOUT:
+        timeout = self.connection_timeout
+    # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
+    start = time.time()
+    try:
+        self._ensure_connected(self._read_pv, timeout=timeout)
+        # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
+    except TimeoutError:
+        if self._destroyed:
+            raise DestroyedError('Signal has been destroyed')
+        raise
+
+def wait_for_connection(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
+    '''Wait for the underlying signals to initialize or connect'''
+    if timeout is DEFAULT_CONNECTION_TIMEOUT:
+        timeout = self.connection_timeout
+    # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
+    start = time.time()
+    self._ensure_connected(self._read_pv, self._write_pv, timeout=timeout)
+    # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
+
+EpicsSignalBase.wait_for_connection = wait_for_connection_base
+EpicsSignal.wait_for_connection = wait_for_connection
+###############################################################################
+
+from ophyd.signal import EpicsSignalBase
+# EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=10)  # old style
+EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)  # new style
+
 
 from csx1.startup import *
 
