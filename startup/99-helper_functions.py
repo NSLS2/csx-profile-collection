4d9ef0b4ea071d6f3e6a845631fbb9023ebde707
Andi Barbour <ambarb@users.noreply.github.com>
2023-12-01 09:44:55 -0500

    initial values to be close

--- startup/99-helper_functions.py
+++ startup/99-helper_functions.py
@@ -109,8 +109,8 @@ def mvslt3(size=None): #TODO make a better version for slt3.pinhole child
     #x Mtr.OFF = 4.88, y Mtr.OFF = -0.95
     holes = {2000: ( -8.79, 0.00),    #TODO eventually have IOC to track these values
                50: (  0.00, 0.00),
-               20: (  8.818, 0.065),
-               10: ( 17.534,-0.055),} 
+               20: (  8.799, 0.060),
+               10: ( 17.534,-0.045),} 
     if size is None:
         xpos = np.round( slt3.x.read()['slt3_x']['value'], 2)
         ypos = np.round( slt3.y.read()['slt3_y']['value'], 2)
