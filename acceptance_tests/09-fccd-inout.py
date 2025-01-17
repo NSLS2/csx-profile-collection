8be698cc847759c4e36b2f17d0a291fbbcec6c15
Padraic Shafer <76011594+padraic-shafer@users.noreply.github.com>
2025-01-17 08:49:53 -0800

    Remove fcc.connected assertion from acceptance test

--- acceptance_tests/09-fccd-inout.py
+++ acceptance_tests/09-fccd-inout.py
@@ -1,8 +1,8 @@
 from bluesky.plans import rel_scan
 #from bluesky.callbacks import LiveTable, LivePlot
 
-
-assert fccd.connected
+# fccd.connected shows as False, but does not prevent running the scans below. 
+# assert fccd.connected
 
 #ct_dark is fccd tests 3 gain modes
 RE(bpp.pchain(count([fccd], num=2), beam_show(), ct_dark_all(2), beam_block()))
