bff626b40728ae0b8ab833a73466bbc5c7928c81
Max Rakitin <mrakitin@bnl.gov>
2025-03-27 01:09:17 -0400

    Pass redis server name using the new `nslsii.configure_base` API

--- startup/00-nsls2-tools.py
+++ startup/00-nsls2-tools.py
@@ -1,7 +1,7 @@
 from ophyd.signal import EpicsSignalBase
+
 EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)  # new style
 # EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=10)  # old style
-
 
 
 import appdirs
@@ -12,15 +12,15 @@ from pathlib import Path
 from csx1.analysis.callbacks import BECwithTicks
 
 ip = get_ipython()
-nslsii.configure_base(ip.user_ns, 'csx', publish_documents_with_kafka=True, bec=False)
+nslsii.configure_base(
+    ip.user_ns,
+    broker_name="csx",
+    redis_url="info.csx.nsls2.bnl.gov",
+    redis_prefix="",
+    publish_documents_with_kafka=True,
+    bec=False,
+)
 nslsii.configure_olog(ip.user_ns)
-
-# Commenting out the DAMA-suggested location
-# (~/.local/share/bluesky/runengine-metadata), as the BL staff prefers a
-# separate location.
-# runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
-runengine_metadata_dir = os.path.expanduser("/nsls2/data/csx/shared/config/RE-metadata")
-RE.md = PersistentDict(runengine_metadata_dir)
 
 bec = BECwithTicks()
 peaks = bec.peaks  # just as alias for less typing
