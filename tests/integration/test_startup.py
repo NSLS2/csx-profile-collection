fe2e69189141bc91d7bf128e78c9911331f0c5a9
thomashopkins32 <thopkins1@bnl.gov>
2025-11-25 09:44:16 -0500

    Finished profile startup testing with mock backend

--- tests/integration/test_startup.py
+++ tests/integration/test_startup.py
@@ -1,66 +1,88 @@
-# tests/conftest.py
-import os
 import sys
 from pathlib import Path
 from unittest.mock import patch, MagicMock
-
-from IPython.core.interactiveshell import InteractiveShell
 import pytest
 import matplotlib
-from ophyd.sim import FakeEpicsSignal, FakeEpicsSignalWithRBV, FakeEpicsSignalRO
 
 # This is needed to prevent matplotlib from trying to use the X server
 matplotlib.use('Agg')
-# CRITICAL: Set this BEFORE importing ophyd or epics
-# This prevents EPICS C library from being loaded
-os.environ['OPHYD_CONTROL_LAYER'] = 'dummy'
 
 
-@pytest.fixture(scope='session', autouse=True)
-def mock_ophyd_signals():
+@pytest.fixture(scope='function', autouse=True)
+def mock_all_ophyd_devices():
+    """
+    Mock EpicsSignalBase methods to prevent any EPICS connections.
+    All signals return 0 for reads and do nothing for writes.
+    """
+    import ophyd
+    
+    def noop(self, *args, **kwargs):
+        return
+    
+    def mock_get(self, *args, **kwargs):
+        return 0
+    
+    def mock_subscribe(self, *args, **kwargs):
+        return 0
 
-    with patch("ophyd.signal.EpicsSignal.wait_for_connection", return_value=True), \
-         patch("ophyd.signal.EpicsSignalBase.wait_for_connection", return_value=True):
-        yield
+    # Save originals
+    originals = {
+        'Device.wait_for_connection': ophyd.Device.wait_for_connection,
+        'EpicsSignal.wait_for_connection': ophyd.signal.EpicsSignal.wait_for_connection,
+        'EpicsSignalBase.wait_for_connection': ophyd.signal.EpicsSignalBase.wait_for_connection,
+        'EpicsSignalBase.get': ophyd.signal.EpicsSignalBase.get,
+        'EpicsSignalBase.put': ophyd.signal.EpicsSignalBase.put,
+        'EpicsSignalBase.subscribe': ophyd.signal.EpicsSignalBase.subscribe,
+        'EpicsSignalBase.set': ophyd.signal.EpicsSignalBase.set,
+    }
+
+    # Apply mocks
+    ophyd.Device.wait_for_connection = noop
+    ophyd.signal.EpicsSignal.wait_for_connection = noop
+    ophyd.signal.EpicsSignalBase.wait_for_connection = noop
+    ophyd.signal.EpicsSignalBase.get = mock_get
+    ophyd.signal.EpicsSignalBase.put = noop
+    ophyd.signal.EpicsSignalBase.subscribe = mock_subscribe
+    ophyd.signal.EpicsSignalBase.set = noop
+    
+    yield
+    
+    # Restore originals
+    ophyd.Device.wait_for_connection = originals['Device.wait_for_connection']
+    ophyd.signal.EpicsSignal.wait_for_connection = originals['EpicsSignal.wait_for_connection']
+    ophyd.signal.EpicsSignalBase.wait_for_connection = originals['EpicsSignalBase.wait_for_connection']
+    ophyd.signal.EpicsSignalBase.get = originals['EpicsSignalBase.get']
+    ophyd.signal.EpicsSignalBase.put = originals['EpicsSignalBase.put']
+    ophyd.signal.EpicsSignalBase.subscribe = originals['EpicsSignalBase.subscribe']
+    ophyd.signal.EpicsSignalBase.set = originals['EpicsSignalBase.set']
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function', autouse=True)
 def mock_nslsii():
-    """
-    Mock NSLS-II specific configuration functions.
-    
-    These try to connect to Kafka, Redis, Olog, etc.
-    """
-    
     def mock_configure_base(ipython_user_ns, beamline_name, **kwargs):
-        """Mock nslsii.configure_base - populates namespace like the real one"""
         from bluesky import RunEngine
         from bluesky.callbacks.best_effort import BestEffortCallback
-        
-        # Create the real objects that nslsii.configure_base would create
         ipython_user_ns['RE'] = RunEngine({})
         ipython_user_ns['db'] = MagicMock()
         ipython_user_ns['sd'] = BestEffortCallback()
         
     with patch('nslsii.configure_base', side_effect=mock_configure_base), \
-         patch('nslsii.configure_olog') as mock_olog:
-        
-        mock_olog.return_value = None
+         patch('nslsii.configure_olog'):
         yield
+
 
 @pytest.fixture
 def startup_dir():
-    """Return the startup directory."""
     profile_dir = Path(__file__).parent.parent.parent
     startup_dir = profile_dir / "startup"
-    
     sys.path.insert(0, str(startup_dir))
     yield startup_dir
     sys.path.remove(str(startup_dir))
 
 
-def test_startup(mock_nslsii, startup_dir):
-    """Test that IPython startup files load without error."""
+def test_startup(startup_dir):
+    from IPython.core.interactiveshell import InteractiveShell
+    
     shell = InteractiveShell.instance()
     try:
         for file in sorted(startup_dir.glob("*.py")):
@@ -69,9 +91,8 @@ def test_startup(mock_nslsii, startup_dir):
             result = shell.run_cell(code, store_history=True, silent=True)
             result.raise_error()
         
-        namespace = shell.user_ns
-        assert "RE" in namespace, "RunEngine not found in namespace"
-        assert "db" in namespace, "Databroker not found in namespace"
+        assert "RE" in shell.user_ns
+        assert "db" in shell.user_ns
     finally:
         InteractiveShell.clear_instance()
     
